726d7b0a7cc9b08ebb1a12ec2b33ba7c4bc22282
Saiedf <saiedf@gmail.com>
2026-04-21 14:05:44 +0200

    Update installer_memorganizeiet5_auto.sh

--- installer_memorganizeiet5_auto.sh
+++ installer_memorganizeiet5_auto.sh
@@ -640,22 +640,19 @@ PKG_FILE=''
 DEB_CANDIDATE=$(pick_deb_file)
 IPK_CANDIDATE=$(pick_ipk_file)
 
-# Universal rule:
-# - DreamOS / Dreambox OE2.5/2.6 -> prefer .deb
-# - OpenATV / OpenPLi / OpenSPA / OpenVIX / OpenBH / OpenVision / Egami / TeamBlue / PurE2 / other open-source images -> prefer .ipk
 if [ "$IMAGE_TYPE" = 'dreamos' ]; then
-    if [ -n "$DEB_CANDIDATE" ]; then
+    if has_deb_support && [ -n "$DEB_CANDIDATE" ]; then
         PKG_TYPE='deb'
         PKG_FILE="$DEB_CANDIDATE"
-    elif [ -n "$IPK_CANDIDATE" ]; then
+    elif has_ipk_support && [ -n "$IPK_CANDIDATE" ]; then
         PKG_TYPE='ipk'
         PKG_FILE="$IPK_CANDIDATE"
     fi
 else
-    if [ -n "$IPK_CANDIDATE" ]; then
+    if has_ipk_support && [ -n "$IPK_CANDIDATE" ]; then
         PKG_TYPE='ipk'
         PKG_FILE="$IPK_CANDIDATE"
-    elif [ -n "$DEB_CANDIDATE" ]; then
+    elif has_deb_support && [ -n "$DEB_CANDIDATE" ]; then
         PKG_TYPE='deb'
         PKG_FILE="$DEB_CANDIDATE"
     fi
